0078565fc5db862916c7617a79b6fe9b7008ab68
Yannick Schutz <hello@yannickschutz.com>
2020-10-30 18:08:01 +0100

    Do not skip invisible

--- themes/less/assets/js/main.js
+++ themes/less/assets/js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   new LazyLoad({
-    elements_selector: ".lazyload"
+    elements_selector: ".lazyload",
+    skip_invisible: false
   });
   mediumZoom(document.querySelectorAll('[data-action="zoom"]'), {margin: 20});
 })
