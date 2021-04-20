93ba5961c0809e9b887970f190f98e37aa5cc5ea
Greg Mignard <49413247+gmignard@users.noreply.github.com>
2021-04-20 15:13:40 +0200

    Zoooooooom (#69)
    * Start the swipe
    
    * Ignore resources
    
    * No resources
    
    * Index
    
    * Replaced
    
    * Article + Zoom
    
    * clickToCloseNonZoomable
    
    * Update main.js
    
    * Force close
    
    * Single pages get it too
    
    * New Home
    
    * New Home
    
    Co-authored-by: Yannick Schutz <hello@yannickschutz.com>

--- themes/less/assets/js/main.js
+++ themes/less/assets/js/main.js
@@ -3,5 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
     elements_selector: ".lazyload",
     skip_invisible: false
   });
-  mediumZoom(document.querySelectorAll('[data-action="zoom"]'), {margin: 20});
+  photoswipeSimplify.init({
+    history: false,
+    bgOpacity: 1,
+    zoomEl: false,
+    shareEl: false,
+    counterEl: false,
+    fullscreenEl: false,
+    closeEl: false,
+    captionEl: false,
+    arrowEl: false,
+    barsSize: { top: 44, bottom: 44, left: 44, right: 44 }
+  });
 })
