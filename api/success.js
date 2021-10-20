240c4fe85f595d1d7552631584127609e61e42b8
Yannick Schutz <hello@yannickschutz.com>
2021-10-20 18:48:47 +0200

    Add Functions

---
+++ api/success.js
@@ -0,0 +1,13 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler =  async(event, context) => {
+  const { session_id } = event.queryStringParameters.query;
+
+  const session = await stripe.checkout.sessions.retrieve(session_id);
+
+  console.log(session);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(session)
+  }
+};
