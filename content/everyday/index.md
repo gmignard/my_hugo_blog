119987d5dc1477bc786d854a865ffae6c8b9f2c0
Yannick Schutz <Yannick.schutz@gmail.com>
2022-01-23 15:39:39 +0100

    tailwind (#99)
    * tailwind
    
    * Build
    
    * Force node
    
    * Stripe
    
    * mgrid
    
    * Everyday
    
    * Home selection
    
    * Menu
    
    * Everyday
    
    * Everyday
    
    Co-authored-by: Greg Mignard <49413247+gmignard@users.noreply.github.com>

---
+++ content/everyday/index.md
@@ -0,0 +1,8 @@
+---
+title: "Everyday - Grégory Mignard"
+subtitle: "Des photos du quotidien"
+seotitle: "Everyday - Des photos du quotidien - Grégory Mignard"
+type: "grid"
+resources:
+- src: "*.jpg"
+---
---
+++ themes/less/tailwind.config.js
@@ -0,0 +1,134 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+  variants: {
+    extend: {
+      typography: ["dark"],
+    }
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: [
+          "-ui-monospace",
+          "SFMono-Regular",
+          "ui-monospace",
+          "Monaco",
+          "Andale Mono",
+          "Ubuntu Mono",
+          "monospace"
+        ],
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Helvetica Neue",
+          "Helvetica",
+          "Arial",
+          "sans-serif"
+        ],
+        serif: [
+          "-apple-system-ui-serif",
+          "Iowan Old Style",
+          "Apple Garamond",
+          "Baskerville",
+          "Times New Roman",
+          "Droid Serif",
+          "Times",
+          "Source Serif Pro",
+          "serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol"
+        ]
+      },
+      colors: {
+        gray: colors.stone,
+        indigo: colors.indigo,
+        teal: colors.teal,
+        orange: colors.orange,
+        amber: colors.amber,
+        slate: colors.slate,
+        rose: colors.rose,
+        red: colors.red,
+        emerald: colors.emerald,
+        sky: colors.sky,
+        dark: "#2d2f34",
+        darker: "#1F2023"
+      },
+      typography: (theme) => ({
+        light: {
+          css: [
+            {
+              color: theme('colors.gray.100'),
+              '[class~="lead"]': {
+                color: theme('colors.gray.300'),
+              },
+              strong: {
+                color: theme('colors.white'),
+              },
+              'ol > li::before': {
+                color: theme('colors.gray.400'),
+              },
+              'ul > li::before': {
+                backgroundColor: theme('colors.gray.600'),
+              },
+              hr: {
+                borderColor: theme('colors.gray.200'),
+              },
+              blockquote: {
+                color: theme('colors.gray.200'),
+                borderLeftColor: theme('colors.gray.600'),
+              },
+              h1: {
+                color: theme('colors.white'),
+              },
+              h2: {
+                color: theme('colors.white'),
+              },
+              h3: {
+                color: theme('colors.white'),
+              },
+              h4: {
+                color: theme('colors.white'),
+              },
+              'figure figcaption': {
+                color: theme('colors.gray.400'),
+              },
+              code: {
+                color: theme('colors.white'),
+              },
+              'a code': {
+                color: theme('colors.white'),
+              },
+              pre: {
+                color: theme('colors.gray.200'),
+                backgroundColor: theme('colors.gray.800'),
+              },
+              thead: {
+                color: theme('colors.white'),
+                borderBottomColor: theme('colors.gray.400'),
+              },
+              'tbody tr': {
+                borderBottomColor: theme('colors.gray.600'),
+              },
+            },
+          ],
+        },
+        DEFAULT: {
+          css: {
+            img: {
+              marginTop: 0,
+              marginBottom: 0
+            }
+          }
+        }
+      }),
+    }
+  }
+}
